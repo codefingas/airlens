ca5b456951aebd4bd2423964ea2a138aeb64c7fd
codefingers <udohezekiel34@yahoo.com>
2019-07-25 23:25:51 +0100

    added files to manipulate with and have initialized the facedetector

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
-
 $(document).ready(function () {
   
+/**initializing the face detection */
+    $('#picture').faceDetection({
+      complete : function (faces) {
+        console.log(faces);//logging the faces to the console
+      }
+
+    });
+/**end of initializing the face detection */
 
 });
